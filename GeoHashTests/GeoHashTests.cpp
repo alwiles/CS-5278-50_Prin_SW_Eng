ef423493fc3ebda071f4aca03e9087a70af647d6
alwiles <amyjacey@yahoo.com>
2024-08-01 15:00:36 -0700

    update test

--- GeoHashTests/GeoHashTests.cpp
+++ GeoHashTests/GeoHashTests.cpp
@@ -1,65 +1,60 @@
 #include "pch.h"
-#include "CppUnitTest.h"
 #include "../GeoHash/GeoHash.h"
+#include <gtest/gtest.h>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace GeoHashTests
 {
-	TEST_CLASS(GeoHashTests)
+	TEST(GeoHashTests, Test1)
 	{
-	public:
-		
-		TEST_METHOD(TestGeoHash1D)
-		{
 			GeoHash MyGeoHash = GeoHash();
 			char geoHashString[6] = { 0 };
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LONGITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("00000", geoHashString);
+			ASSERT_EQ("00000", geoHashString);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LATITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("00000", geoHashString);
+			ASSERT_EQ("00000", geoHashString);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LONGITUDE_RANGE[1], MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("11111", geoHashString);
+			ASSERT_EQ("11111", geoHashString);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[1], MyGeoHash.LATITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("11111", geoHashString);
+			ASSERT_EQ("11111", geoHashString);
 
 			MyGeoHash.GetGeohashString(0, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("10000", geoHashString);
+			ASSERT_EQ("10000", geoHashString);
 
 			MyGeoHash.GetGeohashString(90.0, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("11000", geoHashString);
+			ASSERT_EQ("11000", geoHashString);
 
 			MyGeoHash.GetGeohashString(135.0, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("11100", geoHashString);
+			ASSERT_EQ("11100", geoHashString);
 
 			MyGeoHash.GetGeohashString(157.5, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("11110", geoHashString);
+			ASSERT_EQ("11110", geoHashString);
 
 			MyGeoHash.GetGeohashString(168.75, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("11111", geoHashString);
+			ASSERT_EQ("11111", geoHashString);
 
 			MyGeoHash.GetGeohashString(-1, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("01111", geoHashString);
+			ASSERT_EQ("01111", geoHashString);
 
 			MyGeoHash.GetGeohashString(-91.0, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("00111", geoHashString);
+			ASSERT_EQ("00111", geoHashString);
 
 			MyGeoHash.GetGeohashString(-136.0, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("00011", geoHashString);
+			ASSERT_EQ("00011", geoHashString);
 
 			MyGeoHash.GetGeohashString(-158.5, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("00001", geoHashString);
+			ASSERT_EQ("00001", geoHashString);
 
 			MyGeoHash.GetGeohashString(-169.75, MyGeoHash.LONGITUDE_RANGE, 5, geoHashString);
-			Assert::AreEqual("00000", geoHashString);
+			ASSERT_EQ("00000", geoHashString);
 
 		}
 
-		TEST_METHOD(TestGeoHash2D)
+		TEST(GeoHashTests, TestGeoHash2D)
 		{
 			GeoHash MyGeoHash = GeoHash();
 			char geoHashString10Bit[11] = { 0 };
@@ -67,38 +62,38 @@ namespace GeoHashTests
 			char geoHashString14Bit[15] = { 0 };
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[0], 10, geoHashString10Bit);
-			Assert::AreEqual("0000000000", geoHashString10Bit);
+			ASSERT_EQ("0000000000", geoHashString10Bit);
 			
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[1], 10, geoHashString10Bit);
-			Assert::AreEqual("0101010101", geoHashString10Bit);
+			ASSERT_EQ("0101010101", geoHashString10Bit);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[1], 11, geoHashString11Bit);
-			Assert::AreEqual("01010101010", geoHashString11Bit);
+			ASSERT_EQ("01010101010", geoHashString11Bit);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[0], 11, geoHashString11Bit);
-			Assert::AreEqual("00000000000", geoHashString11Bit);
+			ASSERT_EQ("00000000000", geoHashString11Bit);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[1], -158.5, 10, geoHashString10Bit);
-			Assert::AreEqual("1010101011", geoHashString10Bit);
+			ASSERT_EQ("1010101011", geoHashString10Bit);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[1], -158.5, 11, geoHashString11Bit);
-			Assert::AreEqual("10101010111", geoHashString11Bit);
+			ASSERT_EQ("10101010111", geoHashString11Bit);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[1], -158.5, 14, geoHashString14Bit);
-			Assert::AreEqual("10101010111111", geoHashString14Bit);
+			ASSERT_EQ("10101010111111", geoHashString14Bit);
 
 			MyGeoHash.GetGeohashString(MyGeoHash.LATITUDE_RANGE[1], MyGeoHash.LONGITUDE_RANGE[1], 14, geoHashString14Bit);
-			Assert::AreEqual("11111111111111", geoHashString14Bit);
+			ASSERT_EQ("11111111111111", geoHashString14Bit);
 
 		}
 
-		TEST_METHOD(TestIntegerEncoding)
+		TEST(GeoHashTests, TestIntegerEncoding)
 		{
 			GeoHash MyGeoHash = GeoHash();
 
-			Assert::AreEqual(0, MyGeoHash.GetGeohashInteger(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[0], 10));
-			Assert::AreEqual(682, MyGeoHash.GetGeohashInteger(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[1], 10));
-			Assert::AreEqual(16213, MyGeoHash.GetGeohashInteger(MyGeoHash.LATITUDE_RANGE[1], -158.5, 14));
+			ASSERT_EQ(0, MyGeoHash.GetGeohashInteger(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[0], 10));
+			ASSERT_EQ(682, MyGeoHash.GetGeohashInteger(MyGeoHash.LATITUDE_RANGE[0], MyGeoHash.LONGITUDE_RANGE[1], 10));
+			ASSERT_EQ(16213, MyGeoHash.GetGeohashInteger(MyGeoHash.LATITUDE_RANGE[1], -158.5, 14));
 		}
 	};
-}
+
