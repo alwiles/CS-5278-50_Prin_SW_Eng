5c929264455fc6cdaa6a000696f2ec478da2d9a6
alwiles <amyjacey@yahoo.com>
2024-08-24 23:30:39 -0700

    update test

--- GeoHashTests/GeoDBTests.cpp
+++ GeoHashTests/GeoDBTests.cpp
@@ -1,29 +1,22 @@
 #include "pch.h"
-#include "CppUnitTest.h"
-//#include "../GeoHash/GeoHash.h"
-//#include "../GeoHash/GeoDB.h"
 #include "../GeoHash/GeoDBFactory.h"
+#include <gtest/gtest.h>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace GeoDBTests
 {
-	TEST_CLASS(GeoDBTests)
-	{
-	public:
-
-		TEST_METHOD(TestSimpleInsert)
+		TEST(GeoDBTests, TestSimpleInsert)
 		{
 			int bitsOfPrecision = 16;
 			GeoDB db = GeoDBFactory::CreateDatabase(bitsOfPrecision);
 			db.InsertLocation(0, 0);
 			
 			for (int i = 0; i < bitsOfPrecision; i++) {
-				Assert::IsTrue(db.ContainsLocation(0, 0, i));
+				ASSERT_TRUE(db.ContainsLocation(0, 0, i));
 			}
 		}
 
-		TEST_METHOD(TestSimpleDelete)
+		TEST(GeoDBTests, TestSimpleDelete)
 		{
 			int bitsOfPrecision = 16;
 			GeoDB db = GeoDBFactory::CreateDatabase(bitsOfPrecision);
@@ -34,11 +27,11 @@ namespace GeoDBTests
 			for (int i = 1; i < bitsOfPrecision; i++) { 
 				// this test doesn't make a whole lot of sense, since simple delete uses the max precision
 				// this assert only works because there was only a single entry inserted
-				Assert::IsTrue(!db.ContainsLocation(0, 0, i));
+				ASSERT_TRUE(!db.ContainsLocation(0, 0, i));
 			}
 		}
 
-		TEST_METHOD(TestZeroBits)
+		TEST(GeoDBTests, TestZeroBits)
 		{
 			int bitsOfPrecision = 16;
 			GeoDB db = GeoDBFactory::CreateDatabase(bitsOfPrecision);
@@ -48,10 +41,10 @@ namespace GeoDBTests
 			db.InsertLocation(-90, 180);
 			db.InsertLocation(90, -180);
 			
-			Assert::AreEqual(5.0, (double)db.NearbyLocations(0, 0, 0).size(), .01);
+			ASSERT_FLOAT_EQ(5.0, (double)db.NearbyLocations(0, 0, 0).size());
 		}
 
-		TEST_METHOD(TestZeroBitsDelete)
+		TEST(GeoDBTests, TestZeroBitsDelete)
 		{
 			int bitsOfPrecision = 16;
 			GeoDB db = GeoDBFactory::CreateDatabase(bitsOfPrecision);
@@ -64,10 +57,10 @@ namespace GeoDBTests
 
 			db.DeleteAll(0, 0, 0);
 
-			Assert::AreEqual(0.0, (double)db.NearbyLocations(0, 0, 0).size(), .01);
+			ASSERT_FLOAT_EQ(0.0, (double)db.NearbyLocations(0, 0, 0).size());
 		}
 
-		TEST_METHOD(TestInsertDeleteSeries)
+		TEST(GeoDBTests, TestInsertDeleteSeries)
 		{
 			int bitsOfPrecision = 16;
 			GeoDB db = GeoDBFactory::CreateDatabase(bitsOfPrecision);
@@ -78,24 +71,24 @@ namespace GeoDBTests
 			db.InsertLocation(-90, 180);
 			db.InsertLocation(90, -180);
 
-			Assert::IsTrue(db.ContainsLocation(0, 0, 16));
-			Assert::IsTrue(db.ContainsLocation(90, 180, 16));
-			Assert::IsTrue(db.ContainsLocation(-90, -180, 16));
-			Assert::IsTrue(db.ContainsLocation(-90, 180, 16));
-			Assert::IsTrue(db.ContainsLocation(90, -180, 16));
-			Assert::IsTrue(db.ContainsLocation(90.5, -180.5, 16));
-			Assert::IsTrue(!db.ContainsLocation(1, -1, 16));
-			Assert::IsTrue(!db.ContainsLocation(45, -45, 16));
+			ASSERT_TRUE(db.ContainsLocation(0, 0, 16));
+			ASSERT_TRUE(db.ContainsLocation(90, 180, 16));
+			ASSERT_TRUE(db.ContainsLocation(-90, -180, 16));
+			ASSERT_TRUE(db.ContainsLocation(-90, 180, 16));
+			ASSERT_TRUE(db.ContainsLocation(90, -180, 16));
+			ASSERT_TRUE(db.ContainsLocation(90.5, -180.5, 16));
+			ASSERT_TRUE(!db.ContainsLocation(1, -1, 16));
+			ASSERT_TRUE(!db.ContainsLocation(45, -45, 16));
 			
 			db.DeleteLocation(90, -180);
-			Assert::IsTrue(!db.ContainsLocation(90, -180, 16));
+			ASSERT_TRUE(!db.ContainsLocation(90, -180, 16));
 			
 			db.DeleteAll(1, 1, 1);
-			Assert::IsTrue(db.ContainsLocation(-90, -180, 16));
-			Assert::IsTrue(!db.ContainsLocation(90, 180, 16));
+			ASSERT_TRUE(db.ContainsLocation(-90, -180, 16));
+			ASSERT_TRUE(!db.ContainsLocation(90, 180, 16));
 			
 			db.InsertLocation(90, 180);
-			Assert::IsTrue(db.ContainsLocation(90, 180, 16));
+			ASSERT_TRUE(db.ContainsLocation(90, 180, 16));
 		}
-	};
+	
 }
